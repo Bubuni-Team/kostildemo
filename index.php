48bc19635ddb42bc98f624edc99c42f980c92e59
West14 <30056636+West14@users.noreply.github.com>
2021-06-24 21:26:50 +0300

    Add pdo_mysql to requirements in composer.json. Add requirements check to index.php.

--- index.php
+++ index.php
@@ -7,9 +7,32 @@
  * Made with <3 by West from TechGate Studio
  */
 
+$phpVersion = phpversion();
+if (version_compare($phpVersion, '7.1.0', '<'))
+{
+    die("PHP 7.1.0 or newer is required. $phpVersion does not meet this requirement. Please ask your host to upgrade PHP.");
+}
+
+$requirements = [
+    'JSON' => 'json_encode',
+    'PDO' => 'pdo_drivers'
+];
+foreach ($requirements as $req => $fn)
+{
+    if (!function_exists($fn))
+    {
+        die("$req extension is required.");
+    }
+}
+
+if (!in_array('mysql', PDO::getAvailableDrivers()))
+{
+    die('PDO MySQL driver is required');
+}
+
 $dir = __DIR__;
 
 require_once $dir . '/src/App.php';
 
-$app = new \App();
+$app = new App();
 $app->run($dir);
